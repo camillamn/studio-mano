dc8ab516118c4acafabe5bd7c5d525d8e6cd1238
Camilla <112381378+camillamn@users.noreply.github.com>
2022-11-23 13:08:17 +0100

    Fixed some minor bugs

--- js/modules/navigation.js
+++ js/modules/navigation.js
@@ -4,12 +4,19 @@ export default function Navigation() {
     let navigationVisible = false; 
 
     /* QuerySelectors */
+	 
+	 // create a reference to the Block (BEM) element
+	 const menu = document.querySelector('.header');
     const menuButton = document.querySelector('.header__menu-button');
     const headerNavigation = document.querySelector('.header__navigation');
 
 
     /* Event listeners */
-    menuButton.addEventListener('click', handleMenuButtonClick);
+	 // check if Block exists before adding any other event listeners
+	 // if the main block element exists, so should all other child elements
+	 if (menu) {
+		menuButton.addEventListener('click', handleMenuButtonClick);
+	 }
 
     /* Event Handlers */
     function handleMenuButtonClick(event){
