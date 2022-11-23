2ac9ee0e0745f5f17aefc74ca1f61a17c97a734b
Camilla <112381378+camillamn@users.noreply.github.com>
2022-11-23 14:44:28 +0100

    Added header and menu to all sites

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 import Header from './modules/header.js';
 import Navigation from './modules/navigation.js';
 import Slideshow from './modules/slideshow.js';
+import Footer from './modules/footer.js';
 
 Header();
 Navigation();
 Slideshow();
+Footer();
