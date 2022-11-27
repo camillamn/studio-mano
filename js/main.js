ad6c35221d64c274d691a3ec655e081eb097b321
Camilla <112381378+camillamn@users.noreply.github.com>
2022-11-27 10:58:43 +0100

    Added animated burger-button to all pages

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@ import Header from './modules/header.js';
 import Navigation from './modules/navigation.js';
 import Slideshow from './modules/slideshow.js';
 import Footer from './modules/footer.js';
-// import Hamburger from './modules/hamburger.js';
+import Hamburger from './modules/hamburger.js';
 
 Header();
 Navigation();
 Slideshow();
 Footer();
-// Hamburger();
+Hamburger();
--- js/modules/hamburger.js
+++ js/modules/hamburger.js
@@ -1,3 +1,13 @@
 export default function Hamburger() {
-	
+	const menuButton = document.querySelector('.header__menu-button');
+	let menuVisible = false;
+	menuButton.addEventListener('click', () => {
+		if(!menuVisible) {
+			menuButton.classList.add('header__menu-button--visible');
+			menuVisible = true;
+		} else {
+			menuButton.classList.remove('header__menu-button--visible');
+			menuVisible = false;
+		}
+	});
 }
